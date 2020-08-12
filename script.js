8bb85792b1606077760f51e9c9642d6fb1287445
Haz-git <zhouharry1@gmail.com>
2020-08-12 09:26:05 -0700

    Added Feature To Click On Food Item

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const resultsText = document.getElementById('resultsText');
 const searchBox = document.getElementById('searchBox');
 const form = document.getElementById('form');
 const mealContainer = document.getElementById('meals-container');
+const mealInfoContainer = document.getElementById('mealInfoContainer')
 
 function runSearch(e) {
     e.preventDefault();
@@ -40,8 +41,63 @@ function runSearch(e) {
     }
 }
 
+function findFoodByID(id) {
+    //fetches meal by id number:
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
 
+            addMealToDOM(meal);
+        });
+}
+
+function addMealToDOM(meal) {
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+
+    //adding meal info to html:
+    mealInfoContainer.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal} />
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
+    `;
+}
 
 //Adding Event Listeners
 
 form.addEventListener('submit', runSearch);
+
+mealContainer.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        findFoodByID(mealID);
+    }
+});
